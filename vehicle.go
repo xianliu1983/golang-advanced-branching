8f459d9f33c3722cba815737ddff9f074fb4056b
xian.liu <xian.liu@cariad.technology>
2022-10-28 22:32:47 +0200

    Golang learning: first commit

--- vehicle.go
+++ vehicle.go
@@ -1,13 +1,41 @@
 package main
 
-import (
+type vehicle interface {
+}
 
-)
+type car struct {
+	model, make, typeVehicle string
+}
 
+type truck struct {
+	model, make, typeVehicle string
+}
+
+type bike struct {
+	model, make string
+}
 
 // Values array for the feedback.json file
+type Values struct {
+	Models []Model
+}
 
 // Model array for the feedback.json file
+type Model struct {
+	Name     string
+	Feedback []string
+}
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral  int
+}
+
+var vehicleResult map[string]feedbackResult
+
+var inventory []vehicle
 
 type rating float32
 
@@ -21,7 +49,6 @@ const (
 
 func init() {
 
-/*
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
 		bike{"Iron 1200", "Harley"},
@@ -34,15 +61,15 @@ func init() {
 		car{"Camry", "Toyota", "Sedan"},
 		truck{"F-150", "Ford", "Truck"},
 		truck{"RAM1500", "Dodge", "Truck"}}
-*/
-//	vehicleResult = make(map[string]feedbackResult)
+
+	vehicleResult = make(map[string]feedbackResult)
 
 }
 
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+
 	// Print ratings for the different vehicles
 }
 
